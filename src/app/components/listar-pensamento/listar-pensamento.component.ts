a245c9c82e8628d149d840535adc3b1fc61b617a
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-09-02 00:01:12 -0300

    adicionando roteamento na aplicação

---
+++ src/app/components/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
