c2f6cdd7ccae97d7c420ee770b63f83276881c54
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-09-12 00:10:38 -0300

    implementando interface

--- src/app/components/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento/pensamento';
 
 @Component({
   selector: 'listar-pensamento',
@@ -7,18 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
   
-  listaPensamentos = [
-     {
-       conteudo: 'Passo informações para o componente filho',
-       autoria: 'Componente pai',
-       modelo: 'modelo3'
-     },
-     {
-       conteudo: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat',
-       autoria: 'Componente filho',
-       modelo: 'modelo2'
-     }
-  ];
+  listaPensamentos : Pensamento[] = [];
 
   constructor() { }
 
