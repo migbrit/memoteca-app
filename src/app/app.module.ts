0310bfbbb40b66b1bf91dcf582a1c64cc946ae5c
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-09-12 00:15:27 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { CriarPensamentoComponent } from './components/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './components/listar-pensamento/listar-pensamento.component';
+import { PensamentoComponent } from './components/pensamento/pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentoComponent,
+    ListarPensamentoComponent,
+    PensamentoComponent
   ],
   imports: [
     BrowserModule,
